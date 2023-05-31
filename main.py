a13f00f4eaac2474211fdb1480a965e15f980438
donfortune <donfortunet.df@gmail.com>
2023-05-31 17:39:37 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+class User:
+    def view_hotel(self):
+        pass
+
+class Hotel:
+    def book(self):
+        pass
+
+class ReservationTicket:
+    def get_reserve_ticket(self):
+        pass
+
+
+
+print()
+input()
