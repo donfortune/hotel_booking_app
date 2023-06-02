dcc4dc207c91e7ac306704609228d357c5939cd6
donfortune <donfortunet.df@gmail.com>
2023-06-02 10:27:38 +0100

    updated logic for reservation ticket

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ class User:
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
+        self.name = data.loc[data['id'] == self.hotel_id, "name"].squeeze()
+        self.city = data.loc[data['id'] == self.hotel_id, "city"].squeeze()
+
+
     def available(self):    #checks if there are free rooms in the hotel 
         availability = data.loc[data['id'] == self.hotel_id, "available"].squeeze()
         if availability == "yes":
             return True
         else:
             return False
+        
+
     def book(self):   #books rooms by changing availability to no
         availability = data.loc[data['id'] == self.hotel_id, "available"].squeeze()
         if availability == "no":
@@ -24,10 +30,15 @@ class Hotel:
         
 
 class ReservationTicket:
-    def __init__(self, client_name, hotel_obj):
-        pass
+    def __init__(self, client_name, hotel_obj, city):
+        self.client_name = client_name
+        self.hotel = hotel_obj
+        self.city = city
+
+
     def get_reserve_ticket(self):
-       pass
+       content = f"Mr {self.client_name} has booked {self.hotel.name}  in  {self.city} city successfully"
+       return content
 
 print(data)
 
@@ -35,9 +46,10 @@ hotel_ID = input('Enter Id of hotel:')
 hotel = Hotel(hotel_ID)
 if hotel.available():
     hotel.book()
-    name = input('Enter your name')
-    reservation_ticket = ReservationTicket(name, hotel)
+    name = input('Enter your name:')
+    reservation_ticket = ReservationTicket(client_name=name, hotel_obj=hotel, city=city)
     print(reservation_ticket.get_reserve_ticket())
+    
 else:
     print('Hotel is fully Booked')
     
