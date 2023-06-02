3418ccfd16f5136b3788d005e07e799a849324e0
donfortune <donfortunet.df@gmail.com>
2023-06-02 09:47:59 +0100

    finished logic for the methods

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import pandas as pd
 
-data = pd.read_csv('hotels.csv')
+data = pd.read_csv('hotels.csv', dtype={'id': str}) #make sure your string type 'id' is treated as a string not int
 
 class User:
     def view_hotel(self):
         pass
 
 class Hotel:
-    def __init__(self, id):
-        pass
-    def available(self):
-        pass
-    def book(self):
-        pass
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+    def available(self):    #checks if there are free rooms in the hotel 
+        availability = data.loc[data['id'] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
+    def book(self):   #books rooms by changing availability to no
+        availability = data.loc[data['id'] == self.hotel_id, "available"].squeeze()
+        if availability == "no":
+            data.to_csv('hotels.csv', index=False)
+        #data.loc[data['id'] == self.hotel_id, "available"] = 'no' 
+        #data.to_csv('hotels.csv', index=False)
+        
 
 class ReservationTicket:
     def __init__(self, client_name, hotel_obj):
@@ -20,10 +29,10 @@ class ReservationTicket:
     def get_reserve_ticket(self):
        pass
 
-#print(data)
+print(data)
 
-id = input('Enter Id of hotel:')
-hotel = Hotel(id)
+hotel_ID = input('Enter Id of hotel:')
+hotel = Hotel(hotel_ID)
 if hotel.available():
     hotel.book()
     name = input('Enter your name')
